51bdbb9b6ae0f6fd01cd06ad1a17639158ec8da1
Luca Taddeo <luca_taddeo@hotmail.it>
2021-01-16 18:38:13 +0100

    Add structure  to call server to change volume from preact client

--- RemoteControl/WebClient/src/components/volumebox/index.js
+++ RemoteControl/WebClient/src/components/volumebox/index.js
@@ -12,9 +12,15 @@ class VolumeBox extends Component {
         return (
             <Card title={this.props.title}>
                 <Button type="primary" shape="circle" icon={<SoundOutlined />} />
-                <Slider min={0} max={100} defaultValue={this.props.volume}/>
+                <Slider min={0} max={100} defaultValue={this.props.volume} onChange={this.onVolumeChange.bind()}/>
             </Card>
         );
+    }
+
+    onVolumeChange(value) {
+        if (this.props.onVolumeChange !== undefined) {
+            this.props.onVolumeChange(this.props.title, value);
+        }
     }
 }
 
