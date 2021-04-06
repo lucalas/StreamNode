448af9a546c730d998cedd6c958c83740a13df46
Luca Taddeo <luca_taddeo@hotmail.it>
2021-04-06 23:07:55 +0200

    Add task in prebuild phase to create webapp zip to use an internal resource in .NET app; Add http server listen on every ip enable

--- StreamNode/Services/HttpServerService.cs
+++ StreamNode/Services/HttpServerService.cs
@@ -4,6 +4,8 @@ using EmbedIO.Files;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
 
@@ -23,10 +25,10 @@ namespace StreamNode.Services
         {
             server = new WebServer(o => o
             // TODO retrieve ip from network config 
-                    .WithUrlPrefix($"http://192.168.1.198:{port}")
+                    .WithUrlPrefix($"http://*:{port}")
                     .WithMode(HttpListenerMode.EmbedIO))
                 .WithLocalSessionManager()
-                .WithZipFileStream("/", Assembly.GetExecutingAssembly().GetManifestResourceStream("StreamNode.build.zip"), m => m
+                .WithZipFileStream("/", Assembly.GetExecutingAssembly().GetManifestResourceStream("StreamNode.wwwroot.WebClient.zip"), m => m
                     .WithContentCaching(true))
                 //.WithZipFile("/", "F:/Progetti/StreamNode/StreamNode/build.zip")
                 //.WithStaticFolder("/", "F:/Progetti/StreamNode/StreamNode/WebClient/build/", true, m => m
