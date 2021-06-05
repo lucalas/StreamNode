cf889b0e649afa415101bbeddd590c7688e17dd7
Luca Taddeo <luca_taddeo@hotmail.it>
2021-06-05 23:09:19 +0200

    Add required using

--- StreamNode/Services/Settings/ISettings.cs
+++ StreamNode/Services/Settings/ISettings.cs
@@ -1,11 +1,13 @@
-﻿namespace StreamNode.Services.Settings
+﻿using Config.Net;
+
+namespace StreamNode.Services.Settings
 {
     public interface ISettings
     {
         /**
-            Interface for Config Library to Read Settings
+         * Interface for Config Library to Read Settings
          */
-         [Option(DefaultValue=0)]
+        [Option(DefaultValue=0)]
         int WebSocketPort { get; set; }
         string HttpServerIp { get; set; }
         int HttpServerPort { get; set; }
